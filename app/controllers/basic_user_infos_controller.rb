fbf39548d44a8b4af62ae4246d35e3a439a04471
Steven Ott <StevenKarlOtt@gmail.com>
2020-12-27 23:58:49 -0500

    ugh

--- app/controllers/basic_user_infos_controller.rb
+++ app/controllers/basic_user_infos_controller.rb
@@ -41,7 +41,8 @@ class BasicUserInfosController < ApplicationController
   # PATCH/PUT /basic_user_infos/image
   def update
     if @basic_user_info.update(image: params[:image])
-      image = @basic_user_info.image.service_url
+      image = rails_blob_path(@basic_user_info.image)
+      puts image
       render json: {birthdate: @basic_user_info.birthdate, pronoun: @basic_user_info.pronoun, username: @basic_user_info.username, country:@basic_user_info.country, about_me:@basic_user_info.about_me, user_id: @basic_user_info.user_id, image: image}
     else
       render json: @basic_user_info.errors, status: :unprocessable_entity
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
   get "/search", to: "basic_user_infos#search"
-  put "/basic_user_infos/image/:id", to: "basic_user_infos#updateimage"
   post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
 end
