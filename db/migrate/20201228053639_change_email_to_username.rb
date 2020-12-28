27ee3b3c1df72c9dabf9b6425b404498d1452a2d
Steven Ott <StevenKarlOtt@gmail.com>
2020-12-28 02:07:31 -0500

    Added snackbars and validtion for Username uniqueness

--- db/migrate/20201228053639_change_email_to_username.rb
+++ db/migrate/20201228053639_change_email_to_username.rb
@@ -1,5 +1,6 @@
 class ChangeEmailToUsername < ActiveRecord::Migration[6.1]
   def change
-    rename_column :basic_user_infos, :email, :username
+
+    rename_column :basic_user_infos, :email, :username unless BasicUserInfo.column_names.include?('username')
   end
 end
