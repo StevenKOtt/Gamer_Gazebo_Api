5fb832b6976f68c3f45d8adbfdaba2f7cdd2e7a1
Steven Ott <StevenKarlOtt@gmail.com>
2020-12-27 19:39:57 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
-  
-      resource '*',
+      origins ['http://localhost:3001','https://gamergazebo.com']
+      esource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
